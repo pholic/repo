eebc2e52d3c3deff760254b34b07a763485dda32
Brandon Robinson <brandonrobinson@Brandons-MBP.attlocal.net>
2020-10-28 19:28:13 -0500

    Uploaded New Packages

--- Readme.md
+++ Readme.md
@@ -1,1 +1,1 @@
-This is a CydiaIcon repo.
+This is a Cydia repo.
--- dpkg-gettext.pl
+++ dpkg-gettext.pl
@@ -6,29 +6,29 @@ use warnings;
 use strict;
 
 BEGIN {
-    eval 'use Locale::gettext';
-    if ($@) {
-        eval q{
-            sub _g {
-                return shift;
-            }
-            sub textdomain {
-            }
-            sub ngettext {
-                if ($_[2] == 1) {
-                    return $_[0];
-                } else {
-                    return $_[1];
-                }
-            }
-        };
-    } else {
-        eval q{
-            sub _g {
-                return gettext(shift);
-            }
-        };
-    }
+	eval 'use Locale::gettext';
+	if ($@) {
+		eval q{
+			sub _g {
+				return shift;
+			}
+			sub textdomain {
+			}
+			sub ngettext {
+				if ($_[2] == 1) {
+					return $_[0];
+				} else {
+					return $_[1];
+				}
+			}
+		};
+	} else {
+		eval q{
+			sub _g {
+				return gettext(shift);
+			}
+		};
+	}
 }
 
 use base qw(Exporter);
